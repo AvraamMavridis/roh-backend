ca6d6b9c594fe33f7d76a9dce743d2950e407bcd
AvraamMavridis <avr.mav@gmail.com>
2015-06-04 22:24:22 +0200

    Remove console.log

--- src/database.js
+++ src/database.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var winston = require('winston');
 var logger = new winston.Logger();
 
-mongoose.connect('mongodb://localhost/users');
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function () {
-  console.log('Connection to the database established.')
-});
+// mongoose.connect('mongodb://localhost/users');
+// var db = mongoose.connection;
+//
+// db.on('error', console.error.bind(console, 'connection error:'));
+//
+// db.once('open', function () {
+//   console.log('Connection to the database established.')
+// });
 
 module.exports = mongoose;
