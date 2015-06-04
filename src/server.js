f7c10b80789fe0593ab6a74a2ab33570b1039a0f
AvraamMavridis <avr.mav@gmail.com>
2015-06-04 21:24:54 +0200

    server port

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ var articlesRoutes = require('./routes/articlesRoutes');
 var server = new Hapi.Server();
 
 server.connection({
-    port: process.env.PORT || 8001,
+    port: process.env.PORT || 5000,
     routes: {
       cors: true
     }
