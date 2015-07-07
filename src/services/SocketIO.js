9e13cd093360c6d8291ae7b0b73a34dfe9b692df
AvraamMavridis <avr.mav@gmail.com>
2015-07-07 19:16:19 +0200

    Socket.io polling

--- src/services/SocketIO.js
+++ src/services/SocketIO.js
@@ -8,7 +8,13 @@ var socket = null;
 var NewsAggregatorService = require('../services/NewsAggregatorService');
 
 
+
 var io = require('socket.io')(server.info.port);
+
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
 
 io.on('connection', function(socket){
 
